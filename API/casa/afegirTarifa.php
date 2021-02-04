c878faf454e616f29e1b6fe00ff63b3cf280020f
JoanBinimelis <46893257+JoanBinimelis@users.noreply.github.com>
2021-02-04 10:43:30 +0100

    caagondeu

--- API/casa/afegirTarifa.php
+++ API/casa/afegirTarifa.php
@@ -1,7 +1,7 @@
 <?php
 include_once('../../controlador/controlador_casa.php');
-include_once ('../../models/config/DataBase.php');
-include_once ('../../models/classes/casa/Casa.php');
+include_once('../../models/config/DataBase.php');
+include_once('../../models/classes/casa/Casa.php');
 
 
 if(isset($_POST['idCasa']) && isset($_POST['preuTarifa']) && isset($_POST['dataInici']) && isset($_POST['dataFi']) && isset($_POST['nomTarifa'])){
